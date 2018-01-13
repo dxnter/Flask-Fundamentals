3258b81758b02b61d113e7ca7dbbb9d90ad65e11
Danny <contact@danfoster.io>
2018-01-12 20:38:28 -0600

    ajax-ninjas final

---
+++ ajax_ninjas/static/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("button").click(function () {
+        $(".turtleTitle, turtleImage").remove();
+        var turtle = $(this).val();
+        $(".col-md-12").prepend(`<h1 class="turtleTitle">${turtle}</h1>`)
+        $(".turtleTitle").append(`<img class="turtleImage mx-auto" src="static/img/${turtle}.jpg">`)
+    });
+});
